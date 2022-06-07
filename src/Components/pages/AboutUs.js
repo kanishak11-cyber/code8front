e8d3ec8f9f7ce34586e4fb06ff3fc301d6c31584
Harsh Wasnik <harshwasnik160602@gmail.com>
2022-06-07 19:06:49 +0530

    some responsive modifications

--- src/Components/pages/AboutUs.js
+++ src/Components/pages/AboutUs.js
@@ -4,7 +4,7 @@ import { Button } from "../Button";
 import Whatdrivesus from "../whodiveus/Whatdrivesus";
 import Wahtwhy from "../WhatWhy/Wahtwhy";
 import Ouroffering from "../Ouroffering/Ouroffering";
-
+import classes from "./css/aboutus.module.css";
 export default function AboutUs() {
   return (
     <div>
@@ -12,20 +12,21 @@ export default function AboutUs() {
       <div className="grid-container">
         <div className="grid-item">
           <h1
-            style={{
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: "90px",
-              marginTop: "100px",
-              fontSize: 64,
-              fontFamily: "poppins",
-              fontWeight: 400,
-            }}
+          className={classes.heading}
+            // style={{
+            //   display: "flex",
+            //   alignItems: "center",
+            //   paddingLeft: "90px",
+            //   marginTop: "100px",
+            //   fontSize: 64,
+            //   fontFamily: "poppins",
+            //   fontWeight: 400,
+            // }}
           >
             Who we are?
           </h1>
 
-          <p className="aboutpg">
+          <p className={`${classes.content}  aboutpg`}>
             Code8 is an experiential  learning platform driven by the energy of code and ideas to harness the power of innovation in learners.
             We are building a culture to work on projects while getting upskilled with industry relevant learnings and experience.
 
