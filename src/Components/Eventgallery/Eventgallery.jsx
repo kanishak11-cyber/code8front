af361dcdef008e5c6145e4cc0fb118dd888a05bf
Raman <alexesramon0909@gmail.com>
2022-06-08 14:26:35 +0530

    hone and build responsive

---
+++ src/Components/Eventgallery/Eventgallery.jsx
@@ -0,0 +1,118 @@
+import React from 'react'
+import "./Eventgallery.css"
+
+const Eventgallery = () => {
+    return (
+        <div className='Event-gallery'>
+            <div>
+                <h1>
+                    Event Gallery
+                </h1>
+            </div>
+            <div className="galleryholder">
+                <div className="Frame">
+                    <div className="event-image">
+                        {/* <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a> */}
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+                <div className="Frame">
+                    <div className="event-image">
+                        <a href="https://code8.ggeeks.me">
+                            <img src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/Event1.jpeg?raw=true" className="img-fluid" alt="thumbnail" />
+                        </a>
+                    </div>
+                    <div className="description-of-event">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Eventgallery
---
+++ src/Components/Testimonials/Testimonial.jsx
@@ -0,0 +1,70 @@
+import React from 'react'
+import "./Testimonial.css"
+
+const Testimonial = () => {
+    return (
+        <div className='Testimonial'>
+
+            <div className='Testimonial-head'>
+                <h1>
+                    Testimonial
+                </h1>
+            </div>
+            <h2>
+                Some amazing Experience from Techie Around.
+            </h2>
+
+            <div className="Testimonial-holder">
+                <div className="testimonial-card">
+                    <img id="img-home" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/icons/profile.png?raw=true" ></img>
+                    <div className="Name-holder">
+                        Anirudh
+                    </div>
+                    <img id="img-rating" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/rating.png?raw=true" ></img>
+                    <div className="Testimonial-exp">
+                        Lorem ipsum dolor sit amet,<br></br> consectetur adipiscing
+                        elit.<br></br> Morbi cras est nisl enim,<br></br>{" "}
+                        pellentesque em dolor est <br></br>
+                        habitant. Vitae nunc, dolor<br></br> gravida duis. At et,
+                        quam est <br></br>donec. Consectetur morbi et<br></br>{" "}
+                        dignissim sed amet erat<br></br> augue quis.
+                    </div>
+                </div>
+                <div className="testimonial-card">
+                    <img id="img-home" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/icons/profile.png?raw=true" ></img>
+                    <div className="Name-holder">
+                        Anirudh
+                    </div>
+                    <img id="img-rating" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/rating.png?raw=true" ></img>
+                    <div className="Testimonial-exp">
+                        Lorem ipsum dolor sit amet,<br></br> consectetur adipiscing
+                        elit.<br></br> Morbi cras est nisl enim,<br></br>{" "}
+                        pellentesque em dolor est <br></br>
+                        habitant. Vitae nunc, dolor<br></br> gravida duis. At et,
+                        quam est <br></br>donec. Consectetur morbi et<br></br>{" "}
+                        dignissim sed amet erat<br></br> augue quis.
+                    </div>
+                </div>
+                <div className="testimonial-card">
+                    <img id="img-home" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/icons/profile.png?raw=true" ></img>
+                    <div className="Name-holder">
+                        Anirudh
+                    </div>
+                    <img id="img-rating" src="https://github.com/KapadiaShubham/code8-media/blob/master/home/img/rating.png?raw=true" ></img>
+                    <div className="Testimonial-exp">
+                        Lorem ipsum dolor sit amet,<br></br> consectetur adipiscing
+                        elit.<br></br> Morbi cras est nisl enim,<br></br>{" "}
+                        pellentesque em dolor est <br></br>
+                        habitant. Vitae nunc, dolor<br></br> gravida duis. At et,
+                        quam est <br></br>donec. Consectetur morbi et<br></br>{" "}
+                        dignissim sed amet erat<br></br> augue quis.
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Testimonial
