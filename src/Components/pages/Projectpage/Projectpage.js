4225bd70fb8dad3de25c00293fdd975a59922ecb
Your Name <you@example.com>
2022-08-11 13:38:51 +0530

    fix project page and download page

--- src/Components/pages/Projectpage/Projectpage.js
+++ src/Components/pages/Projectpage/Projectpage.js
@@ -1,57 +1,68 @@
-import React,{useEffect} from 'react'
-import Explorecategories from './Explorecategories'
-import Newsletter from './Newsletter'
-import "./Projectpage.css"
-import Projects from './Projects'
+import React, { useEffect } from "react";
+import Explorecategories from "./Explorecategories";
+import Newsletter from "./Newsletter";
+import "./Projectpage.css";
+import Projects from "./Projects";
 
 const Projectpage = () => {
-    const queryurl = new URLSearchParams(window.location.search)
+  const queryurl = new URLSearchParams(window.location.search);
 
-useEffect(() => {
+  useEffect(() => {
     console.log("inside use effect");
-  const projects = async ()=>{
-    const respo = await fetch("http://localhost:5000/projects",{
-        method:"GET",
-        body:JSON.stringify()
-    })
-    const projectlist = await respo.json()
-    console.log(projectlist);
-    console.log(projectlist.projects)
-  }
-  projects();
-},[])
-    
+    const projects = async () => {
+      const respo = await fetch("http://localhost:5000/projects", {
+        method: "GET",
+        body: JSON.stringify(),
+      });
+      const projectlist = await respo.json();
+      console.log(projectlist);
+      console.log(projectlist.projects);
+    };
+    projects();
+  }, []);
 
-    const level = queryurl.get('domain')
-    return (<div className='Projectpage'>
-        <div className="proj-head">
-            <div className="proj-intro">
-                <div className='proj-page-heading'>{level} Projects</div>
-                <div className="proj-sub-intr">
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit sequi nam nulla et excepturi autem commodi eos tempore, doloribus, facilis ut rem.
-                </div>
-            </div>
-            <div className="proj-face">
-                <img src="/images/Projectpage.png" alt="" />
-            </div>
+  const level = queryurl.get("domain");
+  return (
+    <div className="Projectpage">
+      <div className="proj-head">
+        <div className="proj-intro">
+          <div className="proj-page-heading">{level} Projects</div>
+          <div className="proj-sub-intr">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            sequi nam nulla et excepturi autem commodi eos tempore, doloribus,
+            facilis ut rem.
+          </div>
         </div>
-        <div className="Projects-holder">
-            <Projects/>
-            <Projects/>
-            <Projects/>
-            <Projects/>
+        <div className="proj-face">
+          <img src="/images/Projectpage.png" alt="" />
         </div>
-        <div className="Categories">
-            <h1>Explore our categories</h1>
-            <div className="Category-holder">
-                <Explorecategories topic={"Web Development"} image={"category3"} options={false}/>
-                <Explorecategories topic={"Android Development"} image={"category2"} options={false}/>
-                <Explorecategories topic={"Blockchain"} image={"category1"} options={false}/>
-            </div>
+      </div>
+      <div className="Projects-holder">
+        <Projects />
+      </div>
+      <div className="Categories">
+        <h1>Explore our categories</h1>
+        <div className="Category-holder">
+          <Explorecategories
+            topic={"Web Development"}
+            image={"category3"}
+            options={false}
+          />
+          <Explorecategories
+            topic={"Android Development"}
+            image={"category2"}
+            options={false}
+          />
+          <Explorecategories
+            topic={"Blockchain"}
+            image={"category1"}
+            options={false}
+          />
         </div>
-        <Newsletter/>
+      </div>
+      <Newsletter />
     </div>
-    )
-}
+  );
+};
 
-export default Projectpage
+export default Projectpage;
--- src/Components/pages/Projectpage/Projects.js
+++ src/Components/pages/Projectpage/Projects.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import "./Projects.css"
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./Projects.css";
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 const Projects = () => {
-  return (<div className='Projects'>
-    <div className="Project-name">
-        Amazon Clone
-    </div>
-    <div className="Project-description">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus consectetur consequatur magni! Eveniet nemo nulla, velit minus nostrum magni impedit fugit distinctio.
-    </div>
-    <div className="Project-buttons">
-       <Link to="/project-page"> <button className="View project-but">View Project  <i className="fa-solid fa-arrow-right"></i></button></Link>
-        <Link to="/project-page"><button className="Download project-but">Download Projects  <i className="fa-solid fa-arrow-right"></i></button></Link>
-    </div>
-  </div>
-  )
-}
+  const [set, dataSet] = useState([]);
+  useEffect(() => {
+    async function fetchAPI() {
+      let response = await fetch("http://localhost:5000/projects");
+      response = await response.json();
+      dataSet(response.projects);
+    }
 
-export default Projects
+    fetchAPI();
+  }, []);
+
+  return (
+    <div className="outer">
+      {set.map((curobject) => {
+        const { name, description } = curobject;
+
+        return (
+          <div className="outer">
+            <div className="Projects">
+              <div className="Project-name">{name}</div>
+              <div className="Project-description">{description}</div>
+              <div className="Project-buttons">
+                <Link to={`/project-page?n=${name}`}>
+                  {" "}
+                  <button className="View project-but">
+                    View Project <i className="fa-solid fa-arrow-right"></i>
+                  </button>
+                </Link>
+                <Link to={`/project-page?n=${name}`}>
+                  <button className="Download project-but">
+                    Download Projects{" "}
+                    <i className="fa-solid fa-arrow-right"></i>
+                  </button>
+                </Link>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Projects;
